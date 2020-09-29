b4a9e9a51b4bc116584f3bc6efc27e380333a1bb
imsourceboom <ecmascript.developer@gmail.com>
2020-09-29 14:45:22 +0900

    modify apple-touch-icon

--- components/Favicons/index.js
+++ components/Favicons/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 const Favicons = () => {
   return (
@@ -6,49 +6,33 @@ const Favicons = () => {
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content="white" />
       <meta name="apple-mobile-web-app-title" />
+      <link rel="apple-touch-icon" sizes="57x57" href="/favicons/apple-icon-57x57.png" />
+      <link rel="apple-touch-icon" sizes="60x60" href="/favicons/apple-icon-60x60.png" />
+      <link rel="apple-touch-icon" sizes="72x72" href="/favicons/apple-icon-72x72.png" />
+      <link rel="apple-touch-icon" sizes="76x76" href="/favicons/apple-icon-76x76.png" />
       <link
-        link="apple-touch-icon"
-        size="57x57"
-        href="/favicons/apple-icon-57x57.png"
-      />
-      <link
-        link="apple-touch-icon"
-        size="60x60"
-        href="/favicons/apple-icon-60x60.png"
-      />
-      <link
-        link="apple-touch-icon"
-        size="72x72"
-        href="/favicons/apple-icon-72x72.png"
-      />
-      <link
-        link="apple-touch-icon"
-        size="76x76"
-        href="/favicons/apple-icon-76x76.png"
-      />
-      <link
-        link="apple-touch-icon"
-        size="114x114"
+        rel="apple-touch-icon"
+        sizes="114x114"
         href="/favicons/apple-icon-114x114.png"
       />
       <link
-        link="apple-touch-icon"
-        size="120x120"
+        rel="apple-touch-icon"
+        sizes="120x120"
         href="/favicons/apple-icon-120x120.png"
       />
       <link
-        link="apple-touch-icon"
-        size="144x144"
+        rel="apple-touch-icon"
+        sizes="144x144"
         href="/favicons/apple-icon-144x144.png"
       />
       <link
-        link="apple-touch-icon"
-        size="152x152"
+        rel="apple-touch-icon"
+        sizes="152x152"
         href="/favicons/apple-icon-152x152.png"
       />
       <link
-        link="apple-touch-icon"
-        size="180x180"
+        rel="apple-touch-icon"
+        sizes="180x180"
         href="/favicons/apple-icon-180x180.png"
       />
 
@@ -80,22 +64,10 @@ const Favicons = () => {
       <link rel="shortcut icon" href="/favicons/favicon.ico" />
 
       <meta name="msapplication-TileColor" content="#ffffff" />
-      <meta
-        name="msapplication-TileImage"
-        content="/favicons/ms-icon-70x70.png"
-      />
-      <meta
-        name="msapplication-TileImage"
-        content="/favicons/ms-icon-144x144.png"
-      />
-      <meta
-        name="msapplication-TileImage"
-        content="/favicons/ms-icon-150x150.png"
-      />
-      <meta
-        name="msapplication-TileImage"
-        content="/favicons/ms-icon-310x310.png"
-      />
+      <meta name="msapplication-TileImage" content="/favicons/ms-icon-70x70.png" />
+      <meta name="msapplication-TileImage" content="/favicons/ms-icon-144x144.png" />
+      <meta name="msapplication-TileImage" content="/favicons/ms-icon-150x150.png" />
+      <meta name="msapplication-TileImage" content="/favicons/ms-icon-310x310.png" />
     </>
   );
 };
